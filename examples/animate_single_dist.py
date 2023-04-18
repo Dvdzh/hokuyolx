6a6cd840682c28053731e1415987b2cd1f9535e3
David Zhu <davidzhu@MacBook-Pro-de-David.local>
2023-04-18 18:04:12 +0200

    Change examples

--- examples/animate_single_dist.py
+++ examples/animate_single_dist.py
@@ -2,10 +2,37 @@
 from hokuyolx import HokuyoLX
 import matplotlib.pyplot as plt
 
-DMAX = 10000
+DMIN = 300
+DMAX = 500
+AMIN = 40
+AMAX = 1040
+import logging
+
+# Configuration du niveau de logs
+logging.basicConfig(level=logging.INFO)
+
+# Instanciation d'un objet logger
+logger = logging.getLogger()
+
+# Ajout d'un handler qui envoie les logs sur la sortie standard (console)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+# Utilisation du logger
+logger.info("Message de log à destination de la sortie standard")
+
+
+def nearest(scan):
+    distances = [i[1] for i in scan] 
+    print("Le plus proche est à : {}".format(min(distances)))
+    return 
 
 def update(laser, plot, text):
     timestamp, scan = laser.get_filtered_dist(dmax=DMAX)
+    nearest(scan)
+    print("timestamps, scan :", timestamp, scan) 
+    # data = laser.get_dist(grouping=5)
+    # print(data)
     plot.set_data(*scan.T)
     text.set_text('t: %d' % timestamp)
     plt.draw()
@@ -13,8 +40,9 @@ def update(laser, plot, text):
 
 def run():
     plt.ion()
-    laser = HokuyoLX()
+    laser = HokuyoLX(logger=logger)
     ax = plt.subplot(111, projection='polar')
+    # ax = plt.subplot(111)
     plot = ax.plot([], [], '.')[0]
     text = plt.text(0, 1, '', transform=ax.transAxes)
     ax.set_rmax(DMAX)
--- examples/animate_single_intens.py
+++ examples/animate_single_intens.py
@@ -2,9 +2,35 @@
 from hokuyolx import HokuyoLX
 import matplotlib.pyplot as plt
 
-DMAX = 10000
+DMAX = 2000
 IMIN = 300.
 IMAX = 2000.
+
+bord = 20
+
+
+import logging
+
+# Configuration du niveau de logs
+logging.basicConfig(level=logging.INFO)
+
+# Instanciation d'un objet logger
+logger = logging.getLogger()
+
+# Ajout d'un handler qui envoie les logs sur la sortie standard (console)
+console_handler = logging.StreamHandler()
+logger.addHandler(console_handler)
+
+# Utilisation du logger
+logger.info("Message de log à destination de la sortie standard")
+
+def nearest(scan):
+    distances = [i[1] for i in scan]
+    if not distances :
+        print("Pas d'objet le plus proche")
+        return
+    print("Obstacle le plus proche : ", min(distances))
+    return 
 
 def get_colors(intens):
     max_val = intens.max()
@@ -12,6 +38,9 @@ def get_colors(intens):
 
 def update(laser, plot, text):
     timestamp, scan = laser.get_filtered_intens(dmax=DMAX)
+    scan = scan[bord:-bord]
+    nearest(scan)
+    # print("timestamp, scan :", timestamp, scan)
     plot.set_offsets(scan[:, :2])
     plot.set_array(scan[:, 2])
     text.set_text('t: %d' % timestamp)
@@ -20,7 +49,7 @@ def update(laser, plot, text):
 
 def run():
     plt.ion()
-    laser = HokuyoLX()
+    laser = HokuyoLX(logger=logger)
     ax = plt.subplot(111, projection='polar')
     plot = ax.scatter([0, 1], [0, 1], s=5, c=[IMIN, IMAX], cmap=plt.cm.Greys_r, lw=0)
     text = plt.text(0, 1, '', transform=ax.transAxes)
